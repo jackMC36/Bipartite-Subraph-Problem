357b13def443754387d29c4c6e98f0c9e777647d
jakozik <jakozik@a108pc04l.local.isima.fr>
2025-12-04 17:43:59 +0100

    Validateur : jakozik <jakozik@a108pc04l.local.isima.fr>
     Sur la branche main
     Votre branche est à jour avec 'origin/main'.
    
     Modifications qui seront validées :
    	nouveau fichier : Bibliographie/Artificial Inteligence_ A Modern Approach.pdf
    	modifié :         Bipartite_Subgraph.py
    	modifié :         Etat.py
    	nouveau fichier : "Mod\303\251lisation/Mod\303\251lisation.pdf"
    	nouveau fichier : "Mod\303\251lisation/Mod\303\251lisation.tex"
    
    Need to finish Backtrack-search and rechekc Etat.py.

--- Bipartite_Subgraph.py
+++ Bipartite_Subgraph.py
@@ -1,5 +1,6 @@
 from typing import List, Tuple
 import copy
+import random
 
 import Etat from Etat.py
 ##################################################################################################################################################################
@@ -12,42 +13,48 @@ class Bipartite_Graph:
     def __init__(self, G: Graph):
         '''Le constructeur specifie le graph G=(V,E).'''
         self.G = G
-        self.Etat = Etat(self.G.get_vertices(),[],self.G.get_edges(),0,[],[])
+        self.etat_initial = Etat(self.G,[],[])
 
     def etat_initial(self) -> Etat:
         '''Retourne l'état initial'''
-        return Etat(self.G.get_vertices(),[],self.G.get_edges(),0,[],[])
+        return etat_initial   
     
-    def actions(self, etat: Etat) -> set[int]:
-        '''Retourne les actions qui peuvent etre prise à partir d'un état donner.'''
-        if len(etat.get_X) >= len(etat.get_Y):
-            return set()
-        else:
-            return etat.get_Y()
-    
-    def succ(self, etat: Etat, action: int) -> Etat:
+    def succ(self, etat: Etat, action: int, chosen_set: str) -> Etat:
         '''Retourne l'etat obtenu après avoir executé une action sur un état.'''
-
 
         new_X = etat.get_X().copy()
         new_Y = etat.get_Y().copy()
-        new_n = etat.get_n()+1
-        new_E = etat.get_E()
+
+        if(chosen_set == 'X')
+            if(action in etat.get_X()):
+                raise ValueError
+            new_X.add(action)
         
-        if(action in etat.get_X()):
-            raise ValueError
+        if(chosen_set == 'Y')
+            if(action in etat.get_Y()):
+                raise ValueError
+            new_Y.add(action)
         
-        new_X.add(action)
-        new_Y.discard(action)
-        new_etat = Etat(new_NB,new_B,self.G.get_edges(),new_n, new_B_v,new_C)
+        
+        new_etat = Etat(self.G,new_X,new_Y)
         return new_etat
-        
-    def goal_test(self, etat: Etat) -> bool:
-        '''Verifie si l'etat est un etat but. '''
-        return len(etat.get_X()) == len(etat.get_Y())
- 
+    
+    def backtrack(self,assignement: Etat):
+        if assignement.is_terminal():
+            print("L'etat est terminal\n")
+            return assignement
+
+        var = random.choice(assignement.actions())
+        possible_sets = {"X","Y"}
+        for s in possible_sets:
+            
+
+
+
     def backtracking_search(self):
-        etat_initial = Etat
+        return backtrack(self, self.etat_initial)
+
+        
 
     
 
--- Etat.py
+++ Etat.py
@@ -9,16 +9,15 @@ import Graph.py
 
 class Etat:
     '''
-    Un Etat decrit un Graph G=(V,E). 
+    un Graph G=(V,E). 
     X et Y deux ensembles de sommets.
     E l'ensemble des arretes.
     '''
-    def __init__(self, G: Graph, X: set[int], Y: set[int], E: List[Tuple[int, int]]):
+    def __init__(self, G: Graph, X: set[int], Y: set[int]):
         '''Le constructeur spécifie une liste de sommets X, une liste de sommets Y,  et une liste de arêtes E.'''
         self.G = G
         self.X = set(X)
         self.Y = set(Y)
-        self.E = set(E)
 
     def __str__(self):
         return "------------------------------\n"+"\n"+"Liste des sommets X: " + self.X.__str__() + "\n"+ "Liste de sommets Y" + self.Y.__str__() + "\n" +"------------------------------\n"
@@ -31,21 +30,54 @@ class Etat:
         '''Retourne la sequence de sommets Y.'''
         return self.Y
 
-    def get_E(self) -> set[int]:
+    def get_E(self):
         '''Retourne l'ensemble des arêtes.'''
-        return self.E
+        return self.G.get_edges()
+
+    def actions(self) -> set(int):
+        '''retourne l'ensemble des actions possibles.'''
+        used_vertices = self.X.union(self.Y)
+        total_vertices = set(self.G.get_vertices())
+        return total_vertices.difference(used_vertices)
 
     def is_valid(self) -> bool:
         '''Retourne True si l'état est valide, faux sinon.'''
         G = self.G
         for v in G.get_vertices():
             neighbors = set(G.get_neigbhors(v))
-            in_both_sets = False
             for n in neighbors:
-                in_both_sets = (n in self.X) and (n in self.Y)
+                if (n in self.X) and (n in self.Y):
+                    return False
+
+        for v in self.X:
+            if not(is_X_assignable(v)):
+                return False
+            
+        for v in self.Y:
+            if not(is_Y_assignable(v)):
+                return False
+                
+        return True
+
+    def is_X_assignable(self, value: int) -> bool:
+        '''retourne vrai si v peut être ajouter à X, faux sinon.'''
+        neighbors = set(G.get_neigbhors(value))
+            for n in neighbors:
+                if n in self.X:
+                    return False
+        return True
+
+    def is_Y_assignable(self, value: int) -> bool:
+        '''retourne vrai si v peut être ajouter à X, faux sinon.'''
+        neighbors = set(G.get_neigbhors(value))
+            for n in neighbors:
+                if n in self.Y:
+                    return False
+        return True
+                
 
     def is_terminal(self) -> bool:
-        return len(self.X) 
+        return ((self.X.union(self.Y)) == self.G.V) and self.is_valid()
 
 
         
